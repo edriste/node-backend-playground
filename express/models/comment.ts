031cc1237322ef5b5bbbdc776f478fe8ca23e398
edriste <edris.temori@dynabase.de>
2024-06-28 14:55:31 +0200

    chore: rename bruno collection

--- express/models/comment.ts
+++ express/models/comment.ts
@@ -1,12 +1,26 @@
 import { ObjectId } from "mongodb";
 
 export default class Comment {
+  public _id: ObjectId;
+  public name: string;
+  public email: string;
+  public movie_id: ObjectId;
+  public text: string;
+  public date: Date;
+
   constructor(
-    public _id: ObjectId,
-    public name: string,
-    public email: string,
-    public movie_id: ObjectId,
-    public text: string,
-    public date: Date,
-  ) {}
+    _id: ObjectId,
+    name: string,
+    email: string,
+    movie_id: ObjectId,
+    text: string,
+    date: Date
+  ) {
+    this._id = _id;
+    this.name = name;
+    this.email = email;
+    this.movie_id = movie_id;
+    this.text = text;
+    this.date = date;
+  }
 }
